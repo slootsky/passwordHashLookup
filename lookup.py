1f4af0f43086204fdd2eb37868fee5d6e85d8f6f
Sloot <git@slootsky.org>
2017-09-06 20:06:43 -0400

    added verbose
    without verbose flag, password is never shown

--- lookup.py
+++ lookup.py
@@ -3,6 +3,7 @@ import msvcrt
 from itertools import product
 import hashlib
 import sqlite3
+import argparse
 
 def mixString(istr):
     l = [(c, c.upper()) if not c.isdigit() else (c,) for c in istr.lower()]
@@ -30,26 +31,49 @@ def dbLookup(hash):
                   
     
 
+parser=argparse.ArgumentParser()
+parser.add_argument('-v','--verbose', action="store_true")
+args=parser.parse_args()
+
 conn = sqlite3.connect('sqlite/pwned-passwords.db')
 
 # for testing
 #pwd=u'pAssw0rd'
 #pwd=u'password'
 
-#pwd = raw_itput("What is your best password?").decode(sys.stdin.encoding or locale.getpreferredencoding(True))
+if args.verbose :
+	pwd = raw_input("What is your best password?")
+else:
+	print "What is your best password?",
+	pwd = getpwd()
+	print ""
 
-print "What is your best password?",
-pwd = getpwd().decode(sys.stdin.encoding or locale.getpreferredencoding(True))
-
-print pwd
+pwd=pwd.decode(sys.stdin.encoding or locale.getpreferredencoding(True))	
 
 sha1 = hashlib.sha1()
 sha1.update(pwd)
-print "{} : {} : {}".format(pwd,sha1.hexdigest(), dbLookup(sha1.hexdigest() ) )
+
+if args.verbose:
+	print "{} : {} : {}".format(pwd,sha1.hexdigest(), dbLookup(sha1.hexdigest() ) )
+else:
+	if dbLookup(sha1.hexdigest() ):
+		print "your password was FOUND"
+	else:
+		print "your password was not found"
+
+findCount = 0		
+permutationCount = 0
 for p in mixString(pwd):
+    permutationCount += 1
     if p <> pwd:
         sha1 = hashlib.sha1()
         sha1.update(p)
         if dbLookup(sha1.hexdigest()):
-            print "{} : {} : {}".format(p,sha1.hexdigest(), dbLookup(sha1.hexdigest() ) )
+			findCount += 1
+			if args.verbose :
+				print "{} : {} : {}".format(p,sha1.hexdigest(), dbLookup(sha1.hexdigest() ) )
         
+if findCount > 0 :
+	print "there were {}/{} case insensitive matches of your password".format(findCount,permutationCount)
+else:
+	print "there were no case insensitive matches of your password"
